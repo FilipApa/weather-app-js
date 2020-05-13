784a00777ec3fa900c8413c56bf13d2d3a80b0d5
filip <filipapatovic992@gmail.com>
2020-05-13 10:49:03 +0200

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,3 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const weatherKey = '35db7b18786680dc33b56babd71fd343';
+export const unsplashKey = 'MMNOXXczwK0NHHVMXT2KKmD1m6qG9e3OAnk36wV_PWc';
